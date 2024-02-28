f200ddc3bb92d677bf995f34c9b76db76a87b1ce
mtasPc <mtasflash@163.com>
2024-02-28 17:47:49 +0800

    头像直接使用项目本地头像资源

--- app/components/emoji.tsx
+++ app/components/emoji.tsx
@@ -13,7 +13,9 @@ export function getEmojiUrl(unified: string, style: EmojiStyle) {
   // Whoever owns this Content Delivery Network (CDN), I am using your CDN to serve emojis
   // Old CDN broken, so I had to switch to this one
   // Author: https://github.com/H0llyW00dzZ
-  return `https://fastly.jsdelivr.net/npm/emoji-datasource-apple/img/${style}/64/${unified}.png`;
+  // 直接使用项目内的头像资源文件 --by liangtianfeng
+  return `/emoji-datasource-apple/img/${style}/64/${unified}.png`;
+  // return `https://fastly.jsdelivr.net/npm/emoji-datasource-apple/img/${style}/64/${unified}.png`;
 }
 
 export function AvatarPicker(props: {
